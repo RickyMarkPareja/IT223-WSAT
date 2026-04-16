2ea38eb56dc850331363074c1749dcc7cdf7c6aa
RMPareja <parejarickymarks@gmail.com>
2026-04-16 15:38:59 +0800

    WSAT-M8: Connected PHP API to MySQL Database

--- personal-cv-react/src/App.jsx
+++ personal-cv-react/src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 /* -- Data Arrays and Churva ---------------------------- */
 
@@ -58,6 +59,7 @@ function App() {
         <Skills skills={skills} />
         <Education education={education} />
         <Contact />
+        <ContactList />
 
         <footer>
         <p>&copy; 2026 Ricky Mark Pareja. All rights reserved.</p>
